42262062cd216101c9fac3310e0a02089c1eca13
Jori Lytter <jori.lytter@gmail.com>
2017-06-25 14:24:02 +0300

    Add very initial UI implementation for fetching ruuvitag data from server and rendering it

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_URL = 'http://localhost:3101'
+
+export const api = {
+  ruuvitags: `${API_URL}/ruuvitag`
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-function component() {
-  var element = document.createElement('div')
-  element.innerHTML = 'Hello world'
+import {api} from './config.js'
+
+const createDiv = data => {
+  const element = document.createElement('div')
+  element.innerHTML = data
   return element
 }
 
-document.body.appendChild(component());
+const createDataElements = tagData => {
+  const rootElement = document.createElement('div')
+  rootElement.appendChild(createDiv(tagData.name))
+  rootElement.appendChild(createDiv(`Lämpotila: ${tagData.data.temperature}`))
+  rootElement.appendChild(createDiv(`Ilmanpaine: ${tagData.data.pressure}`))
+  rootElement.appendChild(createDiv(`Ilmankosteus: ${tagData.data.humidity}`))
+  return rootElement
+}
+
+const formatRuuvitagsData = data => {
+  const dataElements = data.map(tagData => createDataElements(tagData))
+  const rootElement = document.createElement('div')
+  dataElements.map(element => rootElement.appendChild(element))
+  return rootElement
+}
+
+fetch(api.ruuvitags)
+  .then(response => response.json())
+  .then(data => formatRuuvitagsData(data))
+  .then(div => document.body.appendChild(div))
