fe460146e873cb2f9b99c5bca346f4f700429af7
Jori Lytter <jori.lytter@gmail.com>
2017-06-29 17:37:00 +0300

    Render ruuvitag data via vuejs template

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
+import Vue from 'vue'
 import {api} from './config.js'
 
-const createDiv = data => {
-  const element = document.createElement('div')
-  element.innerHTML = data
-  return element
-}
-
-const createDataElements = tagData => {
-  const rootElement = document.createElement('div')
-  rootElement.appendChild(createDiv(tagData.name))
-  rootElement.appendChild(createDiv(`Lämpotila: ${tagData.data.temperature}`))
-  rootElement.appendChild(createDiv(`Ilmanpaine: ${tagData.data.pressure}`))
-  rootElement.appendChild(createDiv(`Ilmankosteus: ${tagData.data.humidity}`))
-  return rootElement
-}
-
-const formatRuuvitagsData = data => {
-  const dataElements = data.map(tagData => createDataElements(tagData))
-  const rootElement = document.createElement('div')
-  dataElements.map(element => rootElement.appendChild(element))
-  return rootElement
-}
+const app = new Vue({
+  el: '#app',
+  data: {
+    ruuvitags: 'No ruuvitags found'
+  }
+})
 
 fetch(api.ruuvitags)
   .then(response => response.json())
-  .then(data => formatRuuvitagsData(data))
-  .then(div => document.body.appendChild(div))
+  .then(data => app.ruuvitags = data)
