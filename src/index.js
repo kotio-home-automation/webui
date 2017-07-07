f7b06fe22d9bbcf43989793a9f56f67622618b4f
Jori Lytter <jori.lytter@gmail.com>
2017-07-07 06:45:30 +0300

    Render ruuvitag data via Vue component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import Vue from 'vue'
 import {api} from './config.js'
 
+const tagData = {
+  template: "<div><span>{{tag.name}}</span><span>{{tag.data.temperature}}</span><span>{{tag.data.humidity}}</span><span>{{tag.data.pressure}}</span></div>",
+  props: ['tag'],
+  data: function() {
+    return {}
+  }
+}
+
+
 const app = new Vue({
   el: '#app',
   data: {
-    ruuvitags: 'No ruuvitags found'
+    ruuvitags: 'waiting for data'
+  },
+  components: {
+    'ruuvitag': tagData
   }
 })
 
 fetch(api.ruuvitags)
   .then(response => response.json())
   .then(data => app.ruuvitags = data)
+
+function data() {
+  return fetch(api.ruuvitags)
+    .then(response => response.json())
+}
