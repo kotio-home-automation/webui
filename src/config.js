6538171566d44538ed0ebdeee23c542c2da9a32e
Jori Lytter <jori.lytter@gmail.com>
2017-07-17 13:17:25 +0300

    Add webui configuration for tellstick API's

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const API_URL = 'http://localhost:3101'
 
 export const api = {
-  ruuvitags: `${API_URL}/ruuvitag`
+  ruuvitags: `${API_URL}/ruuvitag`,
+  tellstickSensors: `${API_URL}/tellstick/sensors`,
+  tellstickSwitches: `${API_URL}/tellstick/switches`
 }
